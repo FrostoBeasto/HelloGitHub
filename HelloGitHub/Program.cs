6a7c09d12beedb2a311c416a0e62008eaa7e64b0
tomas.kment <tomas.kment@VT1-09>
2023-03-06 10:42:27 +0100

    Přidejte soubory projektu.

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] zajmy = { "Ahoj, jsem Tomáš(TomBEAST!) , chodím na SŠ Educhem a rád hraju videohry speciálně souls hry" };
+
+Console.WriteLine(" ");
+
+foreach(var item in zajmy)
+{
+    Console.WriteLine(item);
+}
