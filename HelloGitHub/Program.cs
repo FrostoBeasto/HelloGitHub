7d124a6dc9efe6cf6f89c8382df6e11bf79fbcf0
tomas.kment <tomas.kment@VT1-09>
2023-03-06 11:09:53 +0100

    Přidaný text a změněna barva textu

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -1,6 +1,9 @@
-﻿string[] zajmy = { "Ahoj, jsem Tomáš(TomBEAST!) , chodím na SŠ Educhem a rád hraju videohry speciálně souls hry" };
+﻿string[] zajmy = { "Ahoj, jsem Tomáš(TomBEAST!) , chodím na SŠ Educhem a rád hraju videohry speciálně souls hry",
+        "Taky mám rád volejbal a seriály hlavně Anime." };
 
 Console.WriteLine(" ");
+
+Console.ForegroundColor = ConsoleColor.Green;
 
 foreach(var item in zajmy)
 {
